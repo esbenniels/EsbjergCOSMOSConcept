95e6ad15dc2d8f6c260bd4a33623cd1c6fbf3a3b
esbenniels <56279449+esbenniels@users.noreply.github.com>
2023-06-01 12:53:53 +0200

    Disappearing and LOGIN and LOGOUT buttons

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ function populateDropdown() {
 //     })
 // })
 
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || 
+        navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || 
+        navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || 
+        navigator.userAgent.match(/Windows Phone/i)) {
+    document.getElementById("mobile-login-logout").style.display = "flex";
+} else {
+    document.getElementById("mobile-login-logout").style.display = "none";
+}
 
 
 // HTML Argument Handling
